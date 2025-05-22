d65ee125a1ee08e6f5e690af9f7d8c6ebc611378
Tanuja <tanujakhatun292@gmail.com>
2025-05-22 23:39:44 +0530

    First commit - Brush-up project

---
+++ example.js
@@ -0,0 +1,33 @@
+//  console.log("Bismillah ir-Rahman ir-Rahim")
+//  let age= 15 ;
+// if(age>=18){
+//     console.log("vote");
+// }
+// if(age<18){
+//     console.log("note vote");
+// }
+
+// if (age>=18){
+//     console.log("vote");
+// }else{
+//     console.log("not vote");
+// }
+
+// if (age>=65){
+//     console.log("Old");
+// }else if(age>=18){
+//     console.log("Adult");
+// }else if(age>=12){
+//     console.log("Teenage");
+// }else{
+//     console.log("Baby");
+// }
+// let age =prompt ("enter your age")
+// console.log("your age is" + age);
+// let number = prompt(" Enter a number");
+// if(number %5 ===0){
+//     console.log("the number is multiple of 5");
+// }else{
+//     console.log("the number is  not multiple of 5")
+// }
+
---
+++ grade.js
@@ -0,0 +1,16 @@
+/*write a code which can give grades to students according to their Scorec
+90-100 a
+ 70-89 b
+ 60-69 c
+50-59 d
+ 0-49 f*/
+
+let score = 75;
+let grades;
+
+if(score>=90 && score <=100){
+    grades="A";
+}else if(score>=70 && score<=89){
+    grades="B"
+}
+console.log("Your grades is" +" " +grades)

2fc448529ea8df085d58fdec80c8768c0fd51710
Tanuja <tanujakhatun292@gmail.com>
2025-05-24 00:41:54 +0530

    create counter aopp

---
+++ counter_app/script.js
@@ -0,0 +1,21 @@
+let count =0;
+ 
+ function increase(){
+  count++;
+  document.getElementById("count").innerHTML=count;
+ }
+
+ function reset(){
+  count=0;
+  document.getElementById("count").innerHTML=count;
+ }
+
+ function reset(){
+  count=0;
+  document.getElementById("count").innerHTML=count;
+ }
+
+function decrease(){
+  count--;
+  document.getElementById("count").innerHTML=count;
+ }
--- grade.js
+++ grade.js
@@ -1,16 +1,16 @@
-/*write a code which can give grades to students according to their Scorec
-90-100 a
- 70-89 b
- 60-69 c
-50-59 d
- 0-49 f*/
+// /*write a code which can give grades to students according to their Scorec
+// 90-100 a
+//  70-89 b
+//  60-69 c
+// 50-59 d
+//  0-49 f*/
 
-let score = 75;
-let grades;
+// let score = 75;
+// let grades;
 
-if(score>=90 && score <=100){
-    grades="A";
-}else if(score>=70 && score<=89){
-    grades="B"
-}
-console.log("Your grades is" +" " +grades)
+// if(score>=90 && score <=100){
+//     grades="A";
+// }else if(score>=70 && score<=89){
+//     grades="B"
+// }
+// console.log("Your grades is" +" " +grades)
